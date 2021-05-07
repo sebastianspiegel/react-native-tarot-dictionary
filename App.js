aef8ee5b4c798da155060ff49019b123bc7205dc
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-05-07 14:16:44 -0400

    on press navigates to index page

--- App.js
+++ App.js
@@ -2,21 +2,46 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
+const Stack = createStackNavigator();
+
 export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Welcome"
+          component={Welcome}
+        />
+        <Stack.Screen name="Index" component={Index} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const Welcome = ({navigation}) => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Welcome to the Tarot Dictionary</Text>
       <Button color="white"
         onPress={() => {
-          alert('You tapped the button!');
+          navigation.navigate('Index')
         }}
         title="Start"
       />
       <StatusBar style="auto" />
     </View>
   );
+}
+
+const Index = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Index</Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
