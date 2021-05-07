6d219b0a9d660459a4d93dbde84c615b936b8e2f
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-05-07 14:04:08 -0400

    instalations for navigation

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
