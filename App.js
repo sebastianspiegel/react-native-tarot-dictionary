945e90da19d98d6da4574bdd5af5d49a01211f8c
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-05-07 13:57:59 -0400

    welcome screen with button

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Welcome to the Tarot Dictionary</Text>
+      <Button color="white"
+        onPress={() => {
+          alert('You tapped the button!');
+        }}
+        title="Start"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +20,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 30,
   },
 });
