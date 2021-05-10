516b7d3332e6cd5a30c4cebd2c11ce8b272fa9c3
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-05-10 17:29:36 -0400

    index will display list of cards with titles and descriptions

--- components/ListCard.js
+++ components/ListCard.js
@@ -1,17 +1,25 @@
 import React from 'react'
-import {Text, View, Button, StyleSheet} from 'react-native'
+import {Text, View, Button, StyleSheet, SafeAreaView} from 'react-native'
 
 const ListCard = (props) => {
     return (
-      <View>
+      <SafeAreaView style={styles.container}>
           {console.log(props)}
-          <Text style={styles.text}>{props.card.title}</Text>
-      </View>
+          <Text style={styles.textTitle}>{props.card.title}</Text>
+          <Text style={styles.textDes}>{props.card.description}</Text>
+      </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
-    text: {
+    container: {
+        flex: 2,
+    },
+    textDes: {
+        color: 'white',
+        fontSize: 20
+    },
+    textTitle: {
         color: 'white',
         fontSize: 40
     },
